b73b8dd67906ba0be4289b9edfb718223256e569
Nguyen Ngoc Toan <toan16072000@gmail.com>
2020-10-01 22:35:27 +0700

    First commit

---
+++ Caro/Caro/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Caro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnlChessBoard = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pnlInfo = new System.Windows.Forms.Panel();
+            this.pctbLogo = new System.Windows.Forms.PictureBox();
+            this.txbPlayerName = new System.Windows.Forms.TextBox();
+            this.prcbTime = new System.Windows.Forms.ProgressBar();
+            this.txbIP = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pctbMark = new System.Windows.Forms.PictureBox();
+            this.lblDemo = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.pnlInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctbLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctbMark)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlChessBoard
+            // 
+            this.pnlChessBoard.Location = new System.Drawing.Point(9, 12);
+            this.pnlChessBoard.Name = "pnlChessBoard";
+            this.pnlChessBoard.Size = new System.Drawing.Size(549, 460);
+            this.pnlChessBoard.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.pctbLogo);
+            this.panel2.Location = new System.Drawing.Point(564, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(273, 251);
+            this.panel2.TabIndex = 1;
+            // 
+            // pnlInfo
+            // 
+            this.pnlInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlInfo.Controls.Add(this.lblDemo);
+            this.pnlInfo.Controls.Add(this.pctbMark);
+            this.pnlInfo.Controls.Add(this.btnStart);
+            this.pnlInfo.Controls.Add(this.txbIP);
+            this.pnlInfo.Controls.Add(this.prcbTime);
+            this.pnlInfo.Controls.Add(this.txbPlayerName);
+            this.pnlInfo.Location = new System.Drawing.Point(565, 269);
+            this.pnlInfo.Name = "pnlInfo";
+            this.pnlInfo.Size = new System.Drawing.Size(271, 202);
+            this.pnlInfo.TabIndex = 2;
+            // 
+            // pctbLogo
+            // 
+            this.pctbLogo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pctbLogo.BackgroundImage = global::Caro.Properties.Resources.logo;
+            this.pctbLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pctbLogo.Location = new System.Drawing.Point(3, 0);
+            this.pctbLogo.Name = "pctbLogo";
+            this.pctbLogo.Size = new System.Drawing.Size(267, 249);
+            this.pctbLogo.TabIndex = 0;
+            this.pctbLogo.TabStop = false;
+            // 
+            // txbPlayerName
+            // 
+            this.txbPlayerName.Location = new System.Drawing.Point(6, 8);
+            this.txbPlayerName.Name = "txbPlayerName";
+            this.txbPlayerName.Size = new System.Drawing.Size(155, 20);
+            this.txbPlayerName.TabIndex = 0;
+            // 
+            // prcbTime
+            // 
+            this.prcbTime.Location = new System.Drawing.Point(6, 40);
+            this.prcbTime.Name = "prcbTime";
+            this.prcbTime.Size = new System.Drawing.Size(154, 25);
+            this.prcbTime.TabIndex = 1;
+            // 
+            // txbIP
+            // 
+            this.txbIP.Location = new System.Drawing.Point(6, 78);
+            this.txbIP.Name = "txbIP";
+            this.txbIP.Size = new System.Drawing.Size(154, 20);
+            this.txbIP.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(6, 107);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(153, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // pctbMark
+            // 
+            this.pctbMark.Location = new System.Drawing.Point(165, 8);
+            this.pctbMark.Name = "pctbMark";
+            this.pctbMark.Size = new System.Drawing.Size(104, 121);
+            this.pctbMark.TabIndex = 4;
+            this.pctbMark.TabStop = false;
+            // 
+            // lblDemo
+            // 
+            this.lblDemo.AutoSize = true;
+            this.lblDemo.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDemo.Location = new System.Drawing.Point(3, 133);
+            this.lblDemo.Name = "lblDemo";
+            this.lblDemo.Size = new System.Drawing.Size(219, 57);
+            this.lblDemo.TabIndex = 5;
+            this.lblDemo.Text = "Game Caro";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 483);
+            this.Controls.Add(this.pnlInfo);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pnlChessBoard);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Caro";
+            this.panel2.ResumeLayout(false);
+            this.pnlInfo.ResumeLayout(false);
+            this.pnlInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctbLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctbMark)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlChessBoard;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pctbLogo;
+        private System.Windows.Forms.Panel pnlInfo;
+        private System.Windows.Forms.Label lblDemo;
+        private System.Windows.Forms.PictureBox pctbMark;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txbIP;
+        private System.Windows.Forms.ProgressBar prcbTime;
+        private System.Windows.Forms.TextBox txbPlayerName;
+    }
+}
+
---
+++ Caro/Caro/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DrawChessBoard();
+        }
+
+        void DrawChessBoard()
+        {
+            Button firstButton = new Button()
+            {
+                Width = 0, Location = new Point(0, 0)
+            };
+            for (int i = 0; i < Cons.CHESS_BOARD_HEIGHT; i++)
+            {
+                for (int j = 0; j < Cons.CHESS_BOARD_WIDTH; j++)
+                {
+                    Button btn = new Button(){
+                        Width = Cons.CHESS_WIDTH,
+                        Height = Cons.CHESS_HEIGHT,
+                        Location = new Point(firstButton.Location.X + firstButton.Width, firstButton.Location.Y)
+                    };
+                    pnlChessBoard.Controls.Add(btn);
+                    firstButton = btn;
+                }
+                firstButton.Location = new Point(0, firstButton.Location.Y + Cons.CHESS_HEIGHT);
+                firstButton.Width = 0;
+                firstButton.Height = 0;
+            }
+        }
+    }
+}
